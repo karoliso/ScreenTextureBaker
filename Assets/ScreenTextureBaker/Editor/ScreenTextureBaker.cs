f2c1edcac7f9dba9f6086ea5bea839cc17d6d579
Karolis Ozerovas <k.ozerovas@gmail.com>
2019-08-26 17:38:38 +0100

    * Minor optimizations.

--- Assets/ScreenTextureBaker/Editor/ScreenTextureBaker.cs
+++ Assets/ScreenTextureBaker/Editor/ScreenTextureBaker.cs
@@ -15,6 +15,8 @@ public class ScreenTextureBaker : EditorWindow
     private int screenTextureSize = 1024;
     private int spp = 1;
 
+    private Color32 blankPixelColor = new Color32(255, 255, 255, 0);
+
     [MenuItem("Window/Rendering/Screen Texture Baker")]
     public static void ShowWindow()
     {
@@ -34,24 +36,6 @@ public class ScreenTextureBaker : EditorWindow
     }
 
     /// <summary>
-    /// Clears the target Texture2D to white transparent.
-    /// </summary>
-    /// <param name="targetTexture"></param>
-    private void ClearTexture(Texture2D targetTexture)
-    {
-        Color32 resetColor = new Color32(255, 255, 255, 0);
-        Color32[] resetColorArray = targetTexture.GetPixels32();
-
-        for (int i = 0; i < resetColorArray.Length; i++)
-        {
-            resetColorArray[i] = resetColor;
-        }
-
-        targetTexture.SetPixels32(resetColorArray);
-        targetTexture.Apply();
-    }
-
-    /// <summary>
     /// Bakes main camera texture onto the UVs of the model in view.
     /// </summary>
     void BakeTexture()
@@ -65,10 +49,18 @@ public class ScreenTextureBaker : EditorWindow
 
         screenTexture = new Texture2D(screenTextureSize, screenTextureSize, TextureFormat.RGB24, false);
         screenTexture.ReadPixels(new Rect(0, 0, screenTextureSize, screenTextureSize), 0, 0);
+        Color[] screenTexturePixels = screenTexture.GetPixels();
 
         bakingTexture = new Texture2D(bakingTextureSize, bakingTextureSize, TextureFormat.ARGB32, false);
 
-        ClearTexture(bakingTexture);
+        Color[] bakingTexturePixels = bakingTexture.GetPixels();
+
+        for (int i = 0; i < bakingTexturePixels.Length; i++)
+        { 
+            bakingTexturePixels[i] = blankPixelColor;
+        }
+
+        Color[] currentPassPixels = bakingTexturePixels;
 
         for (int i = 0; i < spp; i++)
         {
@@ -81,7 +73,6 @@ public class ScreenTextureBaker : EditorWindow
                 for (int y = 0; y < screenTexture.height; y++)
                 {
                     Ray ray = camera.ScreenPointToRay(new Vector3(x + Random.value, y + Random.value, 0));
-
                     RaycastHit hit;
 
                     if (Physics.Raycast(ray, out hit, 90f))
@@ -90,26 +81,26 @@ public class ScreenTextureBaker : EditorWindow
                         pixelUV.x *= bakingTextureSize;
                         pixelUV.y *= bakingTextureSize;
 
-                        Color currentSample = screenTexture.GetPixel(x, y);
-
-                        if (currentSample.a > 0)
-                        {
-                            Color accumulatedResult = bakingTexture.GetPixel((int)pixelUV.x, (int)pixelUV.y);
-
-                            currentSample = Color.Lerp(accumulatedResult, currentSample, 1.0f / (i + 1.0f));
-                        }
-
-                        bakingTexture.SetPixel((int)pixelUV.x, (int)pixelUV.y, currentSample);
+                        currentPassPixels[bakingTexture.width * (int)pixelUV.y + (int)pixelUV.x] = screenTexturePixels[screenTexture.width * y + x];
                     }
                 }
             }
+
+            for (int n = 0; n < bakingTexturePixels.Length; n++)
+            {
+                bakingTexturePixels[n] = (bakingTexturePixels[n].a <= 0) ? currentPassPixels[n] : Color.Lerp(bakingTexturePixels[n], currentPassPixels[n], 1.0f / (i + 1.0f));
+            }
         }
+
+        bakingTexture.SetPixels(bakingTexturePixels);
         bakingTexture.Apply();
 
         byte[] bytes = bakingTexture.EncodeToPNG();
         File.WriteAllBytes(Application.dataPath + "/ScreenTextureBaker_OutputImage.png", bytes);
 
         camera.targetTexture = null;
+
+        AssetDatabase.Refresh();
 
         EditorUtility.ClearProgressBar();
     }
